48c357f43f16597508fee29e8839d0b760aa8d08
Wesley Yang <57732129+WYang6060@users.noreply.github.com>
2021-09-17 03:12:39 -0500

    build project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
+import { useDispatch, useSelector } from "react-redux";
 import "./styles.css";
+import { login, logout } from "./store/user";
+import { Field, Form, Formik } from "formik";
 
 export default function App() {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.user);
+
+  if (user) {
+    return (
+      <div>
+        Hi, {user.username}!
+        <button onClick={() => dispatch(logout())}>Logout</button>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values) => {
+          dispatch(login(values));
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <Field type="text" name="username" />
+            <Field type="text" name="password" />
+            <button type="submit" disabled={isSubmitting}>
+              Login
+            </button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 }
